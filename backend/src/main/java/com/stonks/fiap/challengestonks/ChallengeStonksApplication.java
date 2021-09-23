a2d45f74c49db2ff10c89da0bf21b1706b259c53
GuhTiagoSilva <gustavo.tiago.gts@outlook.com>
2021-09-23 19:40:33 -0300

    Initial Commit

---
+++ backend/src/main/java/com/stonks/fiap/challengestonks/ChallengeStonksApplication.java
@@ -0,0 +1,13 @@
+package com.stonks.fiap.challengestonks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeStonksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeStonksApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/stonks/fiap/challengestonks/ChallengeStonksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stonks.fiap.challengestonks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeStonksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
